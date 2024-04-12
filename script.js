21e3a9fe6f8c140d8d565bab4cbb378a41c5ee20
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2024-04-12 16:38:21 +0800

    Improved announcement expansion functionality
    Enhanced the announcement expansion feature to
    provide a more seamless visual experience.
    Now, when an announcement is clicked, the rest
    become hidden, ensuring only the selected one is
    visible.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-function RevealHiddenOverflow(d) {
-    d.classList.toggle("truncate")
+function manageUnclickedAnnouncements(unclickedAnnouncements) {
+    console.log(unclickedAnnouncements.length);
+    unclickedAnnouncements.forEach(unclickedAnnouncement => {
+        var parent = unclickedAnnouncement.parentNode;
+        parent.classList.toggle('hide-announcement');
+    });       
+};
+    
+
+function manageClickedAnnouncement(clickedAnnouncement) {
+    clickedAnnouncement.classList.toggle('truncate');
+    
+    var allAnnouncements = document.querySelectorAll('.announcement');
+    var restAnnouncements = Array.from(allAnnouncements).filter(announcement => announcement !== clickedAnnouncement);
+   
+    manageUnclickedAnnouncements(restAnnouncements);
   }
