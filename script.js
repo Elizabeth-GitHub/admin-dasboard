6a11f4de7053383195c77d285760fa4d17aa686e
Elizabeth <122000137+Elizabeth-GitHub@users.noreply.github.com>
2024-04-18 17:05:23 +0800

    Enhanced announcement expansion functionality
    Now, after clicking back to the truncate regime,
     the dividers appear again.

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+const dividers = document.querySelectorAll('#announcements-list li');
+
 function manageUnclickedAnnouncements(unclickedAnnouncements) {
-    console.log(unclickedAnnouncements.length);
     unclickedAnnouncements.forEach(unclickedAnnouncement => {
         var parent = unclickedAnnouncement.parentNode;
-        parent.classList.toggle('hide-announcement');
-    });       
-};
+        parent.classList.toggle('hidden-announcement');
+    });
+
+    dividers.forEach(divider => {
+        const currentDividerVisibility = divider.style.getPropertyValue('--dividerVisibility');
+        const newDividerVisibility = currentDividerVisibility === 'hidden' ? 'visible' : 'hidden';
+        
+        divider.style.setProperty('--dividerVisibility', newDividerVisibility);
+    });
+}
     
 
 function manageClickedAnnouncement(clickedAnnouncement) {
-    clickedAnnouncement.classList.toggle('truncate');
+    clickedAnnouncement.classList.toggle('truncate-announcement');
+    clickedAnnouncement.classList.toggle('main-announcement');
     
     var allAnnouncements = document.querySelectorAll('.announcement');
     var restAnnouncements = Array.from(allAnnouncements).filter(announcement => announcement !== clickedAnnouncement);
